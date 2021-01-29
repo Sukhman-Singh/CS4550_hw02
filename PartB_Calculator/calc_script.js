29e75773cca69ed1c0e8d9dba78c67fcde8af43a
Sukhman Singh <66216468+Sukhman-Singh@users.noreply.github.com>
2021-01-29 14:03:36 -0500

    Update calc_script.js
    Change back to class, took out global variables, fix 0 multiplication issue, and fix "+=" button.

--- PartB_Calculator/calc_script.js
+++ PartB_Calculator/calc_script.js
@@ -1,106 +1,136 @@
-// initial values
-var prevTerm = "";
-var currTerm = "";
-var operation = undefined;
+(function () {
+  "use strict";
 
-var addNumber = function (number) {
-  if (number == "." && this.currTerm.includes(".")) return;
-  this.currTerm = this.currTerm.toString() + number;
-};
+  class Calculator {
+    constructor(prevTerm, currTerm) {
+      this.prevTerm = prevTerm;
+      this.currTerm = currTerm;
+      this.operation = undefined;
+      this.clear;
+    }
 
-var selectOperation = function (op) {
-  // if the current term is empty, there is no operation to carry out
-  // update the current operation and return
-  if (this.currTerm == "") {
-    this.operation = op;
-    return;
+    addNumber(number) {
+      if (number == "." && this.currTerm.includes(".")) return;
+      this.currTerm = this.currTerm.toString() + number.toString();
+    }
+
+    selectOperation(operation) {
+      // if the current and previous term are empty, just return
+      // cannot have an operation without any terms
+      if (this.currTerm === "" && this.prevTerm === "") {
+        return;
+      }
+
+      // if the current term is empty and previous term is not, there is no operation to
+      // carry out update the current operation and return
+      if (this.currTerm === "" && this.prevTerm !== "") {
+        this.operation = operation;
+        return;
+      }
+
+      // if the current term is not empty and previous term is empty,
+      // update the operation, update the previous term and reset the current term
+      if (this.currTerm !== "" && this.prevTerm === "") {
+        this.operation = operation;
+        this.prevTerm = this.currTerm;
+        this.currTerm = "";
+        return;
+      }
+
+      // else if the previous and current terms are not empty,
+      // carry out the calculation
+      // this.currTerm !== "" && this.prevTerm !== ""
+      this.calculate(operation);
+    }
+
+    // carry out the calculation using the current operation and use the
+    // input "nextOperation" for the next calculation
+    calculate(nextOperation) {
+      // parse the values to numbers
+      const prev = parseFloat(this.prevTerm);
+      const curr = parseFloat(this.currTerm);
+
+      if (isNaN(prev) || isNaN(curr)) {
+        return;
+      }
+
+      switch (this.operation.toString()) {
+        case "+=":
+          this.prevTerm = prev + curr;
+          break;
+        case "-":
+          this.prevTerm = prev - curr;
+          break;
+        case "x":
+          this.prevTerm = prev * curr;
+          break;
+        case "÷":
+          this.prevTerm = prev / curr;
+          break;
+      }
+
+      // if the nextOperation is "+=", just use it like an equals button, not plus
+      if (nextOperation == "+=") {
+        this.currTerm = this.prevTerm;
+        this.prevTerm = "";
+        this.operation = undefined;
+      }
+      // set the input "nextOperation" as the new current operation
+      else {
+        this.operation = nextOperation;
+        // reset current term to empty so a new term could be input
+        this.currTerm = "";
+      }
+    }
+
+    // clear the calculator, reset previous term, current term, and operation
+    clear() {
+      this.prevTerm = "";
+      this.currTerm = "";
+      this.operation = undefined;
+    }
+
+    // update the actual HTML and display of the calculator
+    updateDisplay() {
+      // if the operation is not null, include it in the "currExpression" field
+      if (this.operation != null) {
+        document.getElementById("currExpression").innerHTML =
+          this.prevTerm.toString() +
+          " " +
+          this.operation.toString().substring(0, 1);
+
+        // else if there is no operation, only display the previous number
+      } else {
+        document.getElementById("currExpression").innerHTML = this.prevTerm;
+      }
+      // always display the current term
+      document.getElementById("currTerm").innerHTML = this.currTerm;
+    }
   }
 
-  // if the previous term is not empty, carry out the calculation
-  // reset the current term to empty
-  if (this.prevTerm != "") {
-    calculate(op);
-    this.currTerm = "";
-    return;
-  }
+  const calc = new Calculator("", "");
 
-  // else if the previous term is empty and the current term is not
-  // update the operation, update the previous term and reset the current term
-  this.operation = op;
-  this.prevTerm = this.currTerm;
-  this.currTerm = "";
-};
-
-// carry out the calculation using the current operation and use the
-// input "nextOperation" for the next calculation
-var calculate = function (nextOperation) {
-  // parse the values to numbers
-  const prev = parseFloat(this.prevTerm);
-  const curr = parseFloat(this.currTerm);
-
-  if (isNaN(prev) || isNaN(curr)) {
-    return;
-  }
-
-  switch (this.operation.toString()) {
-    case "+=":
-      this.prevTerm = prev + curr;
-      break;
-    case "-":
-      this.prevTerm = prev - curr;
-      break;
-    case "x":
-      this.prevTerm = prev * curr;
-      break;
-    case "÷":
-      this.prevTerm = prev / curr;
-      break;
-  }
-
-  // set the input "nextOperation" as the new current operation
-  this.operation = nextOperation;
-};
-
-// clear the calculator, reset previous term, current term, and operation
-var clear = function () {
-  this.prevTerm = "";
-  this.currTerm = "";
-  this.operation = undefined;
-};
-
-// update the actual HTML and display of the calculator
-var updateDisplay = function () {
-  // if the operation is not null, include it in the "currExpression" field
-  if (this.operation != null) {
-    document.getElementById("currExpression").innerHTML =
-      this.prevTerm.toString() + " " + this.operation.toString();
-
-    // else if there is no operation, only display the previous number
-  } else {
-    document.getElementById("currExpression").innerHTML = this.prevTerm;
-  }
-  // always display the current term
-  document.getElementById("currTerm").innerHTML = this.currTerm;
-};
-
-// Adding event listeners to the "number" buttons
-Array.from(document.getElementsByClassName("number")).forEach(function (num) {
-  num.addEventListener("click", function () {
-    addNumber(num.innerHTML);
-    updateDisplay();
+  // Adding event listeners to the "number" buttons
+  Array.from(document.getElementsByClassName("number")).forEach(function (num) {
+    num.addEventListener("click", function () {
+      calc.addNumber(num.innerHTML);
+      calc.updateDisplay();
+    });
   });
-});
 
-// Adding event listeners to the "operation" buttons
-Array.from(document.getElementsByClassName("operation")).forEach(function (op) {
-  op.addEventListener("click", function () {
-    selectOperation(op.innerHTML);
-    updateDisplay();
+  // Adding event listeners to the "operation" buttons
+  Array.from(document.getElementsByClassName("operation")).forEach(function (
+    op
+  ) {
+    op.addEventListener("click", function () {
+      calc.selectOperation(op.innerHTML);
+      calc.updateDisplay();
+    });
   });
-});
 
-// Adding an event listener to the "clear" button
-document.getElementById("clear").addEventListener("click", function () {
-  clear();
-  updateDisplay();
-});
+  // Adding event listeners to the "clear" button
+  document.getElementById("clear").addEventListener("click", function () {
+    calc.clear();
+    calc.updateDisplay();
+  });
+})();
